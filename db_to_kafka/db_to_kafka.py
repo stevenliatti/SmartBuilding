df32db47f24452f5a6ce4100de5dad4865b8f8e5
steven.liatti <steven.liatti@hesge.ch>
2020-01-07 13:02:23 +0100

    Add little db_to_kafka script

---
+++ db_to_kafka/db_to_kafka.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import logging
+import time
+import sys
+import json
+
+import mysql.connector
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
+
+cnx = mysql.connector.connect(user='user', password='iot', host='db', database='iot')
+cursor = cnx.cursor()
+
+query = "select device_id, room_number, KnxNode.kind, bloc, floor from Device join KnxNode on id = device_id;"
+cursor.execute(query)
+for (device_id, room_number, kind, bloc, floor) in cursor:
+    device = {
+        "device_id": device_id,
+        "room_number": room_number,
+        "kind": kind,
+        "bloc": bloc,
+        "floor": floor
+    }
+    producer.send('db', key=str.encode('knx_db'), value=str.encode(json.dumps(device)))
+    print(device)
+    time.sleep(0.1)
+
+query = "select device_id, room_number, node_id, name from Device join ZwaveNode on id = device_id;"
+cursor.execute(query)
+for (device_id, room_number, node_id, name) in cursor:
+    device = {
+        "device_id": device_id,
+        "room_number": room_number,
+        "node_id": node_id,
+        "name": name
+    }
+    producer.send('db', key=str.encode('zwave_db'), value=str.encode(json.dumps(device)))
+    print(device)
+    time.sleep(0.1)
+
+cursor.close()
+cnx.close()
--- simple_producer_consumer/consumer.py
+++ simple_producer_consumer/consumer.py
@@ -3,7 +3,7 @@
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(bootstrap_servers=['iot.liatti.ch:29092'])
-consumer.subscribe(['test', 'knx', 'zwave'])
+consumer.subscribe(['test', 'db', 'knx', 'zwave'])
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
